61277ff9adbb8373824c859819711475d770d0a3
Anatoliy <masterjourney1@gmail.com>
2021-12-17 14:35:44 +0200

    Characters pagination

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,43 @@
+import { Component } from 'react';
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+
+    state = {
+        charId: null
+    }
+
+    setCharId = id => {
+        this.setState({charId: id})
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
+                    <div className="char__content">
+                        <ErrorBoundary>
+                            <CharList setCharId={this.setCharId} />
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.charId} />
+                        </ErrorBoundary>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,14 +3,16 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
 class CharList extends Component {
 
     state = {
-        chars: null,
+        chars: [],
         loading: true,
-        error: false
+        error: false,
+        charLoading: false,
+        offset: 210,
+        ended: false
     }
 
     marvelService = new MarvelService();
@@ -19,39 +21,50 @@ class CharList extends Component {
         this.getCharacter()
     }
 
-    onCharactersLoaded = chars => {
-        this.setState({chars, loading: false})
+    onCharactersLoaded = newChars => {
+        let ended = false;
+        if (newChars.length < 9) {
+            ended = true;
+        }
+        this.setState(({chars, loading, charLoading, offset}) => ({
+            chars: [...chars, ...newChars], loading: false, charLoading: false, offset: offset + 9, ended: ended
+        }))
+    }
+
+    onCharLoading = () => {
+        this.setState({charLoading: true})
     }
 
     onError = () => {
-        this.setState({error: true})
+        this.setState({error: true, loading: false})
     }
 
-    getCharacter = () => {
-        this.marvelService.getAllCharacters()
+    getCharacter = (offset) => {
+        this.onCharLoading();
+        this.marvelService.getAllCharacters(this.state.offset)
         .then(this.onCharactersLoaded)
         .catch(this.onError)
     }
 
     render() {
-        const {chars, loading, error} = this.state;
+        const {chars, loading, error, charLoading, offset, ended} = this.state;
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
         let items;
         if (chars) {
-            items = chars.map(char => <Char thumbnail={char.thumbnail} charName={char.name}  key={char.name} />)
+            items = chars.map(char => <Char comics={char.comics} thumbnail={char.thumbnail} charName={char.name} key={char.id} setCharId={() => this.props.setCharId(char.id)} />)
         }
 
         const content = !(error || spinner) ? items : null;
 
         return (
             <div className="char__list">
-                <ul className="char__grid">
                 {errorMessage}
                 {spinner}
+                <ul className="char__grid">
                 {content}
                 </ul>
-                <button className="button button__main button__long">
+                <button disabled={charLoading == true ? true: false} style={{filter: charLoading == true ? 'grayscale(.5)': 'unset', display: ended == true ? 'none': 'block'}} onClick={this.getCharacter} className="button button__main button__long">
                     <div className="inner">load more</div>
                 </button>
             </div>
@@ -59,10 +72,10 @@ class CharList extends Component {
     }
 }
 
-const Char = ({thumbnail, charName}) => {
+const Char = ({thumbnail, charName, setCharId}) => {
     const notFound = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
     return (
-        <li className="char__item">
+        <li className="char__item" onClick={setCharId}>
         <img src={thumbnail} alt={charName} style={{objectFit: thumbnail == notFound ? 'fill': 'cover'}}/>
         <div className="char__name">{charName}</div>
     </li>
